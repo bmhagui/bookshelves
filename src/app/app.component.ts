bc2745798ab496034dd7c85047ade71a0365d79f
Bacem Hagui <bmhagui@gmail.com>
2020-04-11 22:58:39 +0200

    End of Angular course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBZjiadRRvrvptsl_p20cU38AormIcnJNc",
+      authDomain: "bookshelves-11adb.firebaseapp.com",
+      databaseURL: "https://bookshelves-11adb.firebaseio.com",
+      projectId: "bookshelves-11adb",
+      storageBucket: "bookshelves-11adb.appspot.com",
+      messagingSenderId: "946825773321",
+      appId: "1:946825773321:web:2c37d7417babebab9293d8",
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
